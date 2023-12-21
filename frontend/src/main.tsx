8aee29cbc00fd28784e7a2f03dad3efce90774b0
Nataly <mnnmaria@gmail.com>
2023-12-21 13:59:14 -0800

    adding vite and tailwindcss functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
+import React from 'react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+const rootEl = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(rootEl);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App/>
+    </React.StrictMode> 
+    );
