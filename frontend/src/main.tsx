d16e3710e6bcc6cc27b70f15bbd6ed544075215e
Nataly <mnnmaria@gmail.com>
2024-01-18 10:34:49 -0800

    Reformatted Files and added tailwind config and vite

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import React from 'react'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const rootEl = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOM.createRoot(rootEl);
-
 root.render(
   <React.StrictMode>
-    <App/>
-    </React.StrictMode> 
-    );
+    <App />
+  </React.StrictMode>
+);
